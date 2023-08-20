c550e1a712ecc95a0a211e464ae8f157b9da8497
6felix <felix.okoronkwo.1@gmail.com>
2023-08-20 14:02:23 +0100

    init

---
+++ wikitopdf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# converts files in metadata to pdf using WikiVim and pandoc
+
+files="$(ls ./metadata)"
+
+if [ -z "$1" ]; then
+  echo "input name."
+  exit
+fi
+
+for file in $files ; do
+  file="./metadata/$file"
+  if [ -f "$file" ]; then
+    if [ "$(echo $file | cut -d '.' -f3)" = 'html' ]; then
+      echo "$file"
+      pandoc "$file" -o "$1.pdf" --css "./metadata/style.css"
+      # mv "./metadata/$1.pdf" .
+    fi
+  fi
+done
